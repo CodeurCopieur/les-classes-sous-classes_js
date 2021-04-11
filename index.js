54c8b16d370179b4bb05ac92dd233c02b6ddf64e
codwerk <louis_widdy@live.fr>
2021-04-11 20:16:40 +0200

    exo 2

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // CLASSES
-
 class Personne {
   constructor(nom, age){
     this.nom = nom,
@@ -13,7 +12,7 @@ class Personne {
 
 const person = new Personne("boss", 25);
 
-console.log(person.bienvenue());
+//console.log(person.bienvenue());
 
 // SOUS-CLASSES
 
@@ -25,4 +24,33 @@ class Fiche extends Personne {
 }
 
 const fich = new Fiche("codwerk", 25, 186);
-console.log(fich);
+//console.log(fich);
+
+class User {
+  constructor(username) {
+    this.username = username
+  }
+}
+
+const firstUser = new User("codwerk");
+console.log(firstUser.username);
+
+class userInfos {
+  constructor(user){
+    this._user= user; //varaiable prive qui ne peut être utiliser en dehors de la class
+  }
+
+  get name(){
+    return this._user;
+  }
+
+  set new_name(update_name) {
+    this._user = update_name;
+  }
+}
+
+
+const new_infos = new userInfos("Dev");
+console.log(new_infos.name);
+new_infos.new_name = "Dev Front";
+console.log(new_infos.name);
